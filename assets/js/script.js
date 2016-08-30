b41a5ce2d300bee2eb47a0b65a21b728001f473f
lgx <991751219@qq.com>
2016-08-30 19:25:06 +0800

    修改JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,11 @@ if ($(window).width() <= 1280) {
 
 // Variables
     tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag2       = $('.HTML5'),
+    tag3       = $('.JS'),
+    tag4       = $('.PS'),
+    tag5       = $('.CISCO'),
+    tag6       = $('.OTHER');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
